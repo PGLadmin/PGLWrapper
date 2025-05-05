7bf5eb9ef40493aed35150176ab5b72c13b13034
jrelliottoh <jrelliottoh@yahoo.com>
2025-05-05 13:13:37 +0300

    Added Python Dir
    Initially includes ESD.py, GlobConst.py, and SgtEsd.py. Long term will include SPEADMD EOS, and PRtc(Jaubert) translations into python.

---
+++ Python/GlobConst.py
@@ -0,0 +1,17 @@
+#DoublePrecision pi,twoPi,fourPi,avoNum,Rgas,RgasCal,kB,half,zeroTol,third,SQRT2
+#Integer nmx,nCritSet
+#Character*1 tabChar
+import math #for pi
+PGLInputDir='C:\\Users\\jarrell.elliot\\source\\repos\\PGLWrapper\\Input\\'
+nmx=55
+pi=math.pi
+twoPi=2.0*pi
+fourPi = 4.0*pi
+half=0.50
+third=1.0/3
+SQRT2=1.414213562373095
+avoNum=602.2140760
+kB=0.013806490
+Rgas=avoNum*kB
+RgasCal=Rgas/4.1840
+zeroTol=1.E-22
